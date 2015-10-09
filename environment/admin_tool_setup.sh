19cbad1ff3886e7f53da8337221bf0277289ef00
Vladimir Mencl <vladimir.mencl@reannz.co.nz>
2015-10-09 16:23:24 +1300

    DjNRO: setup: initialize DB structures
    Add the following to admin_tool_setup.sh to one-off set up the initial environment:
    
    * create databases with syncdb
      * create admin account as part of that
    * run migrate (workaround needed in place)
    * load initial django data (fixtures - what-is + faq pages)
    * run initial fetch-kml from global eduroam
    
    TODO: create initial realm + load sample data

--- environment/admin_tool_setup.sh
+++ environment/admin_tool_setup.sh
@@ -12,3 +12,20 @@
 docker exec -u postgres postgres psql --command="create role $DB_USER with login encrypted password '$DB_PASSWORD' ;"
 docker exec -u postgres postgres psql --command="create database $DB_NAME with owner $DB_USER;"
 
+# Initialize database on the Django side - and create super user
+docker exec -i djnro ./manage.py syncdb <<-EOF
+	yes
+	admin
+	$ADMIN_EMAIL
+	$ADMIN_PASSWORD
+	$ADMIN_PASSWORD
+EOF
+
+docker exec djnro ./manage.py migrate
+
+# load django fixtures - initial data
+docker exec djnro ./manage.py loaddata initial_data/fixtures_manual.xml
+
+# run fetch-kml one-off:
+docker exec djnro ./manage.py fetch_kml
+
