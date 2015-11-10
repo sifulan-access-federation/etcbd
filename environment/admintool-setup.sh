6d416ff7716dd77fe01f5f5f7b9ad102e83d4346
Vladimir Mencl <vladimir.mencl@reannz.co.nz>
2015-11-10 15:18:14 +1300

    admintool-setup: add support for Kubernetes
    * Handle "docker exec" and "kubectl exec" separately
    * Add a --pod podname option to feed input to kubectl exec
    * Receive env-file-name as an argument (instead of hard-coded)
    * Replace all docker exec calls with exec_func call that delegates to either
      docker_exec or kubectl_exec

--- environment/admintool-setup.sh
+++ environment/admintool-setup.sh
@@ -1,21 +1,66 @@
 #!/bin/bash
 
-# Create databases in the Postgres Image
+# Ancillary functions: define separate exec functions for Kubernetes and Docker
+# Both provide uniform interface:
+# exec_func exec-flags container command
+# NOTE: exec-flags must be specified even when empty - add extra ""
 
+function kubectl_exec() {
+  FLAGS=$1
+  shift
+  CONTAINER=$1
+  shift
+  kubectl exec $FLAGS $PODNAME -c $CONTAINER -- "$@"
+}
+# TODO: kubectl exec ... why do -i exec's hang?
+# Wait for fix to kubernetes/kubernetes#13322 to be included in a release
+
+
+function docker_exec() {
+  FLAGS=$1
+  shift
+  CONTAINER=$1
+  shift
+  docker exec $FLAGS -- $CONTAINER "$@"
+}
+
+# This script supports both Docker-compose and Kubernetes
+
+PODNAME=""
+
+if [ "$1" == "--pod" ] ; then
+    shift
+    PODNAME="$1"
+    shift
+    echo "Using Kubernetes pod $PODNAME"
+    function exec_func() { kubectl_exec "$@" ; }
+else
+    echo "No pod name specified, using docker exec"
+    function exec_func() { docker_exec "$@" ; }
+fi
+
+
+if [ $# -eq 0 ] ; then
+    echo "Usage: $0 [--pod podname] environment_file(s).."
+    exit 1
+fi
 # Load the local deployment environment variables
 # (and filtre the syntax to quote the values first)
-eval $( cat localdev_djnro.env | sed 's/=\(.*\)/="\1"/' )
+eval $( cat "$@" | sed 's/=\(.*\)/="\1"/' )
 
+
+
+# Create databases in the Postgres Image
 # Run a command in the Postgres database to create the role and database
 # Equivalant to:
 #   create role djnrodev with login encrypted password 'djnrodev';
 #   create database djnrodev with owner djnrodev;
 
-docker exec postgres gosu postgres psql --command="create role $DB_USER with login encrypted password '$DB_PASSWORD' ;"
-docker exec postgres gosu postgres psql --command="create database $DB_NAME with owner $DB_USER;"
+exec_func "" postgres gosu postgres psql --command="create role $DB_USER with login encrypted password '$DB_PASSWORD' ;"
+exec_func "" postgres gosu postgres psql --command="create database $DB_NAME with owner $DB_USER;"
 
 # Initialize database on the Django side - and create super user
-docker exec -i djnro ./envwrap.sh ./manage.py syncdb <<-EOF
+exec_func -i djnro ./envwrap.sh ./manage.py syncdb <<-EOF
 	yes
 	admin
 	$ADMIN_EMAIL
@@ -23,23 +68,23 @@ docker exec -i djnro ./envwrap.sh ./manage.py syncdb <<-EOF
 	$ADMIN_PASSWORD
 EOF
 
-docker exec djnro ./envwrap.sh ./manage.py migrate
+exec_func "" djnro ./envwrap.sh ./manage.py migrate
 
 # load django fixtures - initial data
-docker exec djnro ./envwrap.sh ./manage.py loaddata initial_data/fixtures_manual.xml
+exec_func "" djnro ./envwrap.sh ./manage.py loaddata initial_data/fixtures_manual.xml
 
 # run fetch-kml one-off:
-docker exec djnro ./envwrap.sh ./manage.py fetch_kml
+exec_func "" djnro ./envwrap.sh ./manage.py fetch_kml
 
 # create initial realm
-docker exec -i djnro ./envwrap.sh ./manage.py shell <<-EOF
+exec_func -i djnro ./envwrap.sh ./manage.py shell <<-EOF
 	from edumanage.models import Realm
 	Realm(country="$REALM_COUNTRY_CODE").save()
 	exit()
 EOF
 
 # Configure the name of the Django site
-docker exec -i djnro ./envwrap.sh ./manage.py shell <<-EOF
+exec_func -i djnro ./envwrap.sh ./manage.py shell <<-EOF
 	from django.contrib.sites.models import Site
 	site = Site.objects.get(name="example.com")
 	site.name="$SITE_PUBLIC_HOSTNAME"
@@ -51,7 +96,7 @@ EOF
 # import initial data
 if [ -n "$REALM_EXISTING_DATA_URL" ] ; then
     # NOTE: this exact spelling
-    docker exec djnro curl -o djnro/institution.xml "$REALM_EXISTING_DATA_URL"
-    docker exec djnro ./envwrap.sh ./manage.py parse_institution_xml --verbosity=0 djnro/institution.xml
+    exec_func "" djnro curl -o djnro/institution.xml "$REALM_EXISTING_DATA_URL"
+    exec_func "" djnro ./envwrap.sh ./manage.py parse_institution_xml --verbosity=0 djnro/institution.xml
 fi
 
