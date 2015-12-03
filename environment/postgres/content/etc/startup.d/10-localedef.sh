b53cfb03e7d12e872ecd82ee8a69223adc7f8b8c
Vladimir Mencl <vladimir.mencl@reannz.co.nz>
2015-12-03 15:53:18 +1300

    Postgres: set LANG and TZ from global config
    * Generate locale with localedef on demand on startup based on LANG.
    * Remove localedef from Dockerfile

---
+++ environment/postgres/content/etc/startup.d/10-localedef.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ -n "$LANG" ] ; then
+    LNG_COUNTRY=$( echo $LANG | cut -d '.' -f 1 )
+    LNG_CTYPE=$( echo $LANG | cut -d '.' -f 2 )
+    if [ -n "$LNG_CTYPE" ] ; then
+        CHARMAP_ARGS="-f $LNG_CTYPE"
+    fi
+    echo localedef -i "$LNG_COUNTRY" -c $CHARMAP_ARGS -A /usr/share/locale/locale.alias $LANG
+    localedef -i "$LNG_COUNTRY" -c $CHARMAP_ARGS -A /usr/share/locale/locale.alias $LANG
+fi
+
