f9361f1854824e31f88731a686ccf6c1fe2466ba
Vladimir Mencl <vladimir.mencl@reannz.co.nz>
2015-12-17 16:18:17 +1300

    Icingaweb2: volume for certs intead of snakeoil
    * Create a VOLUME /etc/apache/certs to hold Apache certs
    * Switch Apache conf to use server.{crt,key} in this dir instead of Snakeoil certs
    * Remove code generating snakeoil certs - and instead create dummy certs in the above location IF no cert is provided

--- environment/icingaweb/content/etc/startup.d/20-generate-snakeoil-certs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Temporary workaround: generate snakeoil certificates
-
-make-ssl-cert generate-default-snakeoil
-
-
---
+++ environment/icingaweb/content/etc/startup.d/50-make-selfsigned-certs-if-none-present.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+CERT_DIR=/etc/apache2/certs
+CERT=$CERT_DIR/server.crt
+KEY=$CERT_DIR/server.key
+
+if [ ! -e "$CERT" -a ! -e "$KEY" ] ; then
+    OLD_UMASK=$( umask )
+    umask 0077
+    openssl req -new -x509 -sha256 -days 3650 -set_serial $RANDOM -extensions v3_req -out $CERT -keyout $KEY -nodes <<-EOF
+	.
+	.
+	
+	.
+	
+	$SITE_PUBLIC_HOSTNAME
+	
+	EOF
+	# Country
+	# State
+	# Locality
+	# Organization
+	# Organizational Unit
+	# Common name
+	# Email address
+    umask $OLD_UMASK
+fi
+
