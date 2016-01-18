152ad7aca1b4563a997b53b58be9f16ea1ab911a
Vladimir Mencl <vladimir.mencl@reannz.co.nz>
2016-01-18 13:37:12 +1300

    Icinga: check_eduroam: pass all rad_eap_test opts
    Add options to check_eduroam_login.pl to pass through to rad_eap_test:
    
        -v|--verbose    Activate verbose mode
        -m|--method     Auth method [Default: WPA-EAP]
        -e|--eapmethod  EAP method [Default: PEAP]
        -2|--phase2     Phase2 authentication [Default: PAP]
        -O|--operator   Operator name
        -C|--requestcui Request Chargable user identity
        -I|--nasipaddr  NAS IP Address

--- environment/icinga/content/usr/local/lib/nagios/plugins/check_eduroam_login.pl
+++ environment/icinga/content/usr/local/lib/nagios/plugins/check_eduroam_login.pl
@@ -14,6 +14,7 @@ my $version =	"20131106";
 my $host =	"localhost";
 my $port =	1812;
 my $debug =	0;
+my $verbose =	0;
 my $w =		3;
 my $c =		5;
 my $t =		10;
@@ -24,6 +25,14 @@ my $pass;
 my $status;
 
 my $radtest =	"/usr/local/bin/rad_eap_test";
+
+
+my $eduroam_method = "WPA-EAP";
+my $eduroam_eap_method = "PEAP";
+my $eduroam_phase2 = "PAP";
+my $eduroam_operator_name;
+my $eduroam_request_cui;
+my $nas_ip_address;
 
 my %ERRORS =	('OK'=>0,'WARNING'=>1,'CRITICAL'=>2,'UNKNOWN'=>3,'DEPENDENT'=>4);
 
@@ -38,6 +47,7 @@ print <<DATA;
 	$basename [-h] [-d] [-H hostname] [-P port] [-w warning] [-c critical] [-t timeout] -u user -p pass -s secret
 
 	-h|--help	This help screen
+	-v|--verbose	Activate verbose mode
 	-d|--debug	Activate debug mode
 	-H|--hostname	Hostname to send query [Default: $host]
 	-P|--port	Port where status server is listening [Default: $port]
@@ -47,6 +57,12 @@ print <<DATA;
 	-u|--user	Username
 	-p|--pass	Password
 	-s|--secret	Secret
+	-m|--method	Auth method [Default: $eduroam_method]
+	-e|--eapmethod	EAP method [Default: $eduroam_eap_method]
+	-2|--phase2	Phase2 authentication [Default: $eduroam_phase2]
+	-O|--operator	Operator name
+	-C|--requestcui	Request Chargable user identity
+	-I|--nasipaddr	NAS IP Address
 
 	The plugin output performance data about elapsed time executing the query.
 
@@ -58,11 +74,14 @@ DATA
 sub check_options () {
 	my $o_help;
 	my $o_debug;
+	my $o_verbose;
+	my $o_request_cui;
 
 	Getopt::Long::Configure ("bundling");
 	GetOptions(
 		'h|help'	=> \$o_help,
 		'd|debug'	=> \$o_debug,
+		'v|verbose'	=> \$o_verbose,
 		'H|hostname:s'	=> \$host,
 		'P|port:i'	=> \$port,
 		'w|warning:i'	=> \$w,
@@ -71,11 +90,19 @@ sub check_options () {
 		'u|user:s'	=> \$user,
 		'p|pass:s'	=> \$pass,
 		's|secret:s'	=> \$secret,
+		'm|method:s'	=> \$eduroam_method,
+		'e|eapmethod:s'	=> \$eduroam_eap_method,
+		'2|phase2:s'	=> \$eduroam_phase2,
+		'O|operator:s'	=> \$eduroam_operator_name,
+		'C|requestcui'	=> \$o_request_cui,
+		'I|nasipaddr:s'	=> \$nas_ip_address,
 
 	);
 
 	usage() if (defined($o_help));
 	$debug = 1 if (defined($o_debug));
+	$verbose = 1 if (defined($o_verbose));
+	$eduroam_request_cui = 1 if (defined($o_request_cui));
 	if ( $port !~ /^\d+$/ or ($port <= 0 or $port > 65535)) {
 		print "\nPlease insert an integer value between 1 and 65535\n";
 		usage();
@@ -111,7 +138,13 @@ sub check_options () {
 #
 check_options();
 
-my $cmd = "$radtest -u $user -p $pass -H $host -P $port -S $secret -m WPA-EAP -e PEAP -t $t >/dev/null 2>&1 ";
+my $cmd = "$radtest -u $user -p $pass -H $host -P $port -S $secret -m $eduroam_method -e $eduroam_eap_method -2 $eduroam_phase2 -t $t ";
+$cmd .= " -O $eduroam_operator_name" if defined($eduroam_operator_name);
+$cmd .= " -I $nas_ip_address" if defined($nas_ip_address);
+$cmd .= " -C" if $eduroam_request_cui;
+$cmd .= " -v" if $verbose;
+$cmd .= " -c" if $debug;
+$cmd .= ">/dev/null 2>&1 " if !$debug && !$verbose;
 print "DEBUG: radclient command: $cmd\n" if $debug;
 
 my $t0 = [gettimeofday];
