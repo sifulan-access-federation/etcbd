8a718b6e97f79e8af5ed701cd5a0e9ffa2e28f74
Vladimir Mencl <vladimir.mencl@reannz.co.nz>
2016-02-25 14:34:28 +1300

    DjNRO: add locale creation add startup
    (Both for locale set in LANG and for el_GR.UTF-8 used in some parts of the DjNRO codebase)

---
+++ environment/djnro/content/etc/startup.d/20-greek-localedef.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Hard-coded localedef: el_GR.UTF-8
+# DjNRO code-base uses this locale in a few places - better we have it defined on the system
+LNG_COUNTRY=el_GR
+LNG_CTYPE=UTF-8
+CHARMAP_ARGS="-f $LNG_CTYPE"
+localedef -i "$LNG_COUNTRY" -c $CHARMAP_ARGS -A /usr/share/locale/locale.alias $LANG
+
