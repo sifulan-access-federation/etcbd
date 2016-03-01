46624654dce2b7a8c25d1b82cfe640345a178e5a
Vladimir Mencl <vladimir.mencl@reannz.co.nz>
2016-03-02 11:37:15 +1300

    Icinga: adjust workaround for icingaweb startup
    Still without explanation why it's needed, adjust the workaround to make icingaweb start again...

--- environment/icingaweb/content/etc/startup.d/00-workaround-icingaweb2.sh
+++ environment/icingaweb/content/etc/startup.d/00-workaround-icingaweb2.sh
@@ -2,5 +2,5 @@
 
 # workaround an issue with docker+icingaweb2 - module conf not visible until the directory is touched
 
-ls -l /etc/icingaweb2/modules/ > /dev/null
+ls -lR /etc/icingaweb2/modules/ /etc/icingaweb2/enabledModules/ > /dev/null
 
