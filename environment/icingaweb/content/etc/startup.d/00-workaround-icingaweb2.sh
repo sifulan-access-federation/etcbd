9b1fe55d3224e9e0120b1204f0cacd7faf449a31
Vladimir Mencl <vladimir.mencl@reannz.co.nz>
2016-03-02 15:38:39 +1300

    Icingaweb: add comment to workaround (PHP realpath)

--- environment/icingaweb/content/etc/startup.d/00-workaround-icingaweb2.sh
+++ environment/icingaweb/content/etc/startup.d/00-workaround-icingaweb2.sh
@@ -2,5 +2,8 @@
 
 # workaround an issue with docker+icingaweb2 - module conf not visible until the directory is touched
 
+# this may have to do with realpath cache in PHP - accessing all files under
+# /etc/icingaweb2 solves the problem
+
 ls -lR /etc/icingaweb2/modules/ /etc/icingaweb2/enabledModules/ > /dev/null
 
