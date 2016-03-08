de6d4a78fade7c72f4a05acef0dc715539fa6ae1
Vladimir Mencl <vladimir.mencl@reannz.co.nz>
2016-03-09 12:01:06 +1300

    DjNRO: add mgmt cmd to gen Icinga2 configuration

---
+++ environment/djnro/content/djnro/edumanage/management/commands/monconf.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*- vim:encoding=utf-8:
+# vim: tabstop=4:shiftwidth=4:softtabstop=4:expandtab
+import warnings
+
+from optparse import make_option
+from django.core.management.base import BaseCommand
+from edumanage.models import InstServer, InstRealmMon
+from django.template.loader import render_to_string
+
+class Command(BaseCommand):
+    option_list = BaseCommand.option_list + (
+        make_option(
+            '--output',
+            dest='output',
+            default="yaml",
+            help="Output type: json, yaml"
+        ),
+    )
+    args = ''
+    help = "Export monitoring configuration"
+    nro_servers = ( 
+        { 'name': "nro1",
+          'host': "nro1.host",
+          'port': "1812",
+          'secret': "s3cr3t",
+          'status_server': True,
+        },
+        { 'name': "nro2",
+          'host': "nro2.host",
+          'port': "1812",
+          'secret': "s3cr3t",
+          'status_server': True,
+        },
+        ) 
+    def handle(self, *args, **options):
+        self.stdout.write(
+                render_to_string('exports/icinga2_nro.conf',
+                    {
+                     'instrealmmons': InstRealmMon.objects.all(),
+                     'nroservers': self.nro_servers
+                    }
+                )
+            )
+
