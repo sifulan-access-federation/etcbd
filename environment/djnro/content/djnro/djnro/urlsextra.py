7061624de53cf64a2d1dc0fe811d4d0c4decc253
Vladimir Mencl <vladimir.mencl@reannz.co.nz>
2016-03-11 12:58:59 +1300

    DjNRO: urlsextra: pass views as callables not strs
    To be future-proof for Django110

--- environment/djnro/content/djnro/djnro/urlsextra.py
+++ environment/djnro/content/djnro/djnro/urlsextra.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, include, url
+import edumanage.viewsextra
 
 urlpatterns = patterns(
     '',
-    url(r'^icingaconf/?', 'edumanage.viewsextra.icingaconf', name="icingaconf"),
+    url(r'^icingaconf/?', edumanage.viewsextra.icingaconf, name="icingaconf"),
     url(r'^', include('djnro.urls')),
 )
