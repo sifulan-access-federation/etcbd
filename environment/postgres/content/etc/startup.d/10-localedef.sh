5f12b99d446dfc7239a11ebe989101a414ed9908
Vladimir Mencl <vladimir.mencl@reannz.co.nz>
2016-04-04 14:33:00 +1200

    Postgres: create PG DB locale if != LANG locale

--- environment/postgres/content/etc/startup.d/10-localedef.sh
+++ environment/postgres/content/etc/startup.d/10-localedef.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
-if [ -n "$LANG" ] ; then
-    LNG_COUNTRY=$( echo $LANG | cut -d '.' -f 1 )
-    LNG_CTYPE=$( echo $LANG | cut -d '.' -f 2 )
+function create_locale() {
+    TGT_LANG="$1"
+    LNG_COUNTRY=$( echo "$TGT_LANG" | cut -d '.' -f 1 )
+    LNG_CTYPE=$( echo "$TGT_LANG" | cut -d '.' -f 2 )
     if [ -n "$LNG_CTYPE" ] ; then
         CHARMAP_ARGS="-f $LNG_CTYPE"
     fi
-    localedef -i "$LNG_COUNTRY" -c $CHARMAP_ARGS -A /usr/share/locale/locale.alias $LANG
+    localedef -i "$LNG_COUNTRY" -c $CHARMAP_ARGS -A /usr/share/locale/locale.alias "$TGT_LANG"
+}
+
+if [ -n "$LANG" ] ; then
+    create_locale "$LANG"
 fi
 
+# If the PostgreSQL database was created with a different locale, create these locales too - otherwise, PostgreSQL server would not start.
+if [ -f /var/lib/postgresql/data/postgresql.conf ] ; then
+    for PG_LANG in $( grep ^lc_ /var/lib/postgresql/data/postgresql.conf | cut -d "'" -f 2 ) ; do
+        if [ "$PG_LANG" != "$LANG" ] ; then
+          create_locale "$PG_LANG"
+        fi
+    done
+fi
