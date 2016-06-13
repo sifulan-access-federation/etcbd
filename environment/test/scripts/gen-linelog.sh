fde2a65ccfc701f4f42ecb49e135ba0eca870c87
Vladimir Mencl <vladimir.mencl@reannz.co.nz>
2016-06-14 11:55:09 +1200

    ELK/Test: script to generate test linelog messages

---
+++ environment/test/scripts/gen-linelog.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+INST_NAMES="reannz.co.nz standstone.ac.nz sandstone.edu.au limestone.ac.nz limestone.edu.au"
+
+for IDP_INST in $INST_NAMES ; do
+  for SP_INST in $INST_NAMES ; do
+    echo "\"$( date +'%F %T' )\",\"Access-Accept\",\"$( echo $IDP_INST | shasum | cut -f 1 -d ' ' )\",\"$SP_INST\",\"127.0.0.1\",\"127.0.0.1\",\"testing-client\",\"user@$IDP_INST\""
+  done
+done
+
+
+# "2016-01-13 12:39:43","Access-Accept","496d42c40d40af717d9ea02c4b248a09ff58757a","reannz.co.nz","210.7.47.27","210.7.40.57","reannz.ac.nz-1","sam@sandstone.edu.au"
+
+
