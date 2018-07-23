7d706a2188cd8e5f5924ac075294a81ed7bf1477
Vlad Mencl <vladimir.mencl@reannz.co.nz>
2018-07-23 14:32:12 +1200

    Djnro: Django 1.11: drop depr url.pattern

--- environment/djnro/content/djnro/djnro/urlsextra.py
+++ environment/djnro/content/djnro/djnro/urlsextra.py
@@ -1,8 +1,7 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 import edumanage.viewsextra
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'^icingaconf/?', edumanage.viewsextra.icingaconf, name="icingaconf"),
     url(r'^', include('djnro.urls')),
-)
+]
